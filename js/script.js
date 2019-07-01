afffff90b84f6de43bf442c0b8cb5092933eebcd
junior <zukisanante@gmail.com>
2019-07-01 16:50:02 +0200

    js updated

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ const key = "4014ad52db0336b19314ff5bd1e76dbc";
 const apiURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4014ad52db0336b19314ff5bd1e76dbc";
 const weatherDisplay = document.getElementsByClassName('show-weather');
 let weatherFocus = weatherDisplay.value;
-let userInput = document.getElementsByClassName('user-city')
+let userInput = document.getElementsByClassName('user-city');
