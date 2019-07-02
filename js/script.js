0e3abb7e17ee65caea96ed162da1e9365ba01d89
junior <zukisanante@gmail.com>
2019-07-02 09:21:53 +0200

    button, input field added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Declaring variables
 const key = "4014ad52db0336b19314ff5bd1e76dbc";
 const apiURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4014ad52db0336b19314ff5bd1e76dbc";
-const weatherDisplay = document.getElementsByClassName('show-weather');
+const weatherDisplay = document.getElementsByClassName('show-weather'); //div weather
 let weatherFocus = weatherDisplay.value;
-let userInput = document.getElementsByClassName('user-city');
+let userInput = document.getElementsByClassName('user-city'); //input field
